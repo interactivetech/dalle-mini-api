b1a605e70a9107b623f6f6505432f008e542498b
Andrew Mendez <mendeza@Andrews-MacBook-Pro.local>
2022-03-08 23:09:18 -0500

    update

--- cpu/src/app.py
+++ cpu/src/app.py
@@ -76,8 +76,8 @@ def exceptions(e):
 
 if __name__ == '__main__':
     handler = RotatingFileHandler('app.log', maxBytes=100000, backupCount=3)
-    logger = logging.getLogger('tdm')
-    logger.setLevel(logging.INFO)
+    # logger = logging.getLogger('tdm')
+    app.logger.setLevel(logging.INFO)
     app.logger.addHandler(handler)
     app.run(
         host="0.0.0.0",
