e89fd7031517aadcb81c96c4a87eef7749c6eba3
Andrew Mendez <mendeza@Andrews-MacBook-Pro.local>
2022-03-08 22:40:15 -0500

    update

--- cpu/src/app.py
+++ cpu/src/app.py
@@ -11,6 +11,11 @@ import io
 import numpy as np
 from model import load_model, generate_images, load_model2
 
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask, request, jsonify
+from time import strftime
+import traceback
 
 model,tokenizer,vqgan,clip,processor,model_params, vqgan_params, clip_params = load_model2()
 app = Flask(__name__)
@@ -55,8 +60,25 @@ def generate():
             return jsonify({'prompt':input_json['prompt'],'result': encoded_images})
         except:
             jsonify({"sorry": "Sorry, no results! Please try again."}), 500
-    
+
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    logger.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
+
+@app.errorhandler(Exception)
+def exceptions(e):
+    tb = traceback.format_exc()
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    logger.error('%s %s %s %s %s 5xx INTERNAL SERVER ERROR\n%s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, tb)
+    return e.status_code
+
 if __name__ == '__main__':
+    handler = RotatingFileHandler('app.log', maxBytes=100000, backupCount=3)
+    logger = logging.getLogger('tdm')
+    logger.setLevel(logging.ERROR)
+    logger.addHandler(handler)
     app.run(
         host="0.0.0.0",
         port=80
